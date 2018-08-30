6309ca2713f42d0c2141d0ce70a2beb7dec3b058
Mehdi Lahmam <mehdi@lahmam.com>
2018-08-30 23:08:52 +0200

    Update the script to support mruby with the minimum mrbgems
    I needed the following mrbgems:
    
    ```
    +  conf.gem :github => 'iij/mruby-env'
    +  conf.gem :github => 'mattn/mruby-curl'
    +  conf.gem :github => 'k0kubun/mruby-yaml'
    ```

---
+++ README.md
@@ -0,0 +1,19 @@
+# GitLab no-hire-countries watcher
+
+This script reads the [countries list](https://gitlab.com/gitlab-com/www-gitlab-com/blob/8e17aa630ba08102ef6aa4331f65fa01e119352a/data/country_no_hire.yml) where GitLab can't currently hire employees and contractors,
+and will notify me by email when `France` is removed from this list.
+
+I took the opportunity building it to use mruby and to run it on AWS Lambda.
+
+## Necessary mrbgems when compiling mruby
+
+Refer to https://github.com/mruby/mruby/blob/master/doc/guides/compile.md for
+compiling mruby instructions.
+
+I needed the following mrbgems:
+
+```
++  conf.gem :github => 'iij/mruby-env'
++  conf.gem :github => 'mattn/mruby-curl'
++  conf.gem :github => 'k0kubun/mruby-yaml'
+```
--- gitlab_no_hire_countries_watcher.rb
+++ gitlab_no_hire_countries_watcher.rb
@@ -1,31 +1,29 @@
-require 'yaml'
-require 'open-uri'
-
-unless GitLab.no_hire_countries.include?('France')
-  Emailer.call
-end
-
-
-BEGIN {
-
 class GitLab
   COUNTRIES_NO_HIRE_URL = 'https://gitlab.com/gitlab-com/www-gitlab-com/raw/8e17aa630ba08102ef6aa4331f65fa01e119352a/data/country_no_hire.yml'
 
   def self.no_hire_countries
-    YAML.load(open(COUNTRIES_NO_HIRE_URL))
+    YAML.load(Curl.get(COUNTRIES_NO_HIRE_URL).body)
   end
 end
 
 class Emailer
   def self.call
-    Net::HTTP.post_form(
-      URI("https://api:#{ENV.fetch('MAILGUN_API_KEY')}@api.eu.mailgun.net/v3/#{ENV.fetch('MAILGUN_DOMAIN_NAME')}/messages"),
-      from: 'bot@lahmam.com',
-      to: 'mehdi@lahmam.com',
-      subject: 'GitLab is hiring in France 🎉',
-      text: 'Looks like France has been removed from the no-hire list: https://gitlab.com/gitlab-com/www-gitlab-com/blob/master/data/country_no_hire.yml.'
+    Curl.post(
+      "https://api:#{ENV.fetch('MAILGUN_API_KEY')}@api.eu.mailgun.net/v3/#{ENV.fetch('MAILGUN_DOMAIN_NAME')}/messages",
+      [
+        'from=bot@lahmam.com',
+        'to=mehdi@lahmam.com',
+        'subject=GitLab%20is%20hiring%20in%20France%20%F0%9F%8E%89',
+        'text=Looks%20like%20France%20has%20been%20removed%20from%20the%20no-hire%20list%3A%20https%3A%2F%2Fgitlab.com%2Fgitlab-com%2Fwww-gitlab-com%2Fblob%2Fmaster%2Fdata%2Fcountry_no_hire.yml.'
+      ].join('&'),
+      {
+        'Content-Type' => 'application/x-www-form-urlencoded'
+      }
     )
+
   end
 end
 
-}
+unless GitLab.no_hire_countries.include?('France')
+  Emailer.call
+end
